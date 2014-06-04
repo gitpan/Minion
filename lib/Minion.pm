62eca9740b8842be1acc257ddcc19084987fa108
Sebastian Riedel <kraihx@googlemail.com>
2014-06-04 14:18:28 +0000

    Import of SRI/Minion-0.15 from CPAN.
    gitpan-cpan-distribution: Minion
    gitpan-cpan-version:      0.15
    gitpan-cpan-path:         SRI/Minion-0.15.tar.gz
    gitpan-cpan-author:       SRI
    gitpan-cpan-maturity:     released

--- lib/Minion.pm
+++ lib/Minion.pm
@@ -9,7 +9,7 @@ use Mojo::Server;
 use Mojo::URL;
 use Scalar::Util 'weaken';
 
-our $VERSION = '0.14';
+our $VERSION = '0.15';
 
 has app => sub { Mojo::Server->new->build_app('Mojo::HelloWorld') };
 has [qw(auto_perform backend)];
@@ -43,7 +43,7 @@ sub job {
   return undef unless my $job = $self->backend->job_info($id);
   return Minion::Job->new(
     args   => $job->{args},
-    id     => $id,
+    id     => $job->{id},
     minion => $self,
     task   => $job->{task}
   );
